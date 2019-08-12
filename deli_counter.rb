a5293ceee094d85f111b54f124c22eca1e095c58
Michael Lynch <michael@michaelrlynch.com>
2019-08-12 05:55:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,7 @@ def line(katz_deli)
     katz_deli.each_with_index do |name, index|
     print "The line is currently "
     puts "#{index + 1}. #{name} "
+  end
   else
     puts "The line is currently empty."
   end
